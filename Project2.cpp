7c81c8355e9fa79d792643c0cf738f63e7d590c8
acostacos <33932255+acostacos@users.noreply.github.com>
2019-04-15 07:38:21 +0800

    Initial File
    Input receiving

---
+++ Project2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct Program{
+	int needed;
+	vector<int> required;
+	Program(){
+		
+	}
+};
+
+int main(){
+	int numTest;
+	cin >> numTest;
+
+	for(int a=0; a<numTest; a++){
+		int numProc; numType;
+		cin >> numProc;
+		cin >> numType;
+
+		for(int b=0; b<numProc; b++){
+			for(int c=0; c<numType; c++){
+
+			}
+		}
+	}
+
+	return 0;
+}
