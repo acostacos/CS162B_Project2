ecb2696fab84429a2eb6108716f37f79532b9c76
acostacos <33932255+acostacos@users.noreply.github.com>
2019-04-18 11:06:22 +0800

    Changed to accept input (still needs testing)

--- Project2.cpp
+++ Project2.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 struct Program{
-	int needed;
-	vector<int> required;
+	vector<int> current; //number of programs it currently has
+	vector<int> required; //number of programs needed for it to run
 	Program(){
-		
+		current.resize(0);
+		required.resize(0);
 	}
+
+	Program(int a, int b){
+		current.resize(a);
+		required.resize(b);
+	}
+
 };
 
 int main(){
@@ -19,10 +27,37 @@ int main(){
 		cin >> numProc;
 		cin >> numType;
 
-		for(int b=0; b<numProc; b++){
-			for(int c=0; c<numType; c++){
+		Program procs[numProc];
 
+		for(int b=0; b<numProc; b++){
+			Program p(numType, numType);
+			for(int c=0; c<numType; c++){
+				int tempC;
+				cin >> tempC;
+				p.current[c] = tempC;
 			}
+			procs[b] = p;
+		}
+
+		for(int b=0; b<numProc; b++){
+			Program p = procs[b];
+			for(int c=0; c<numType; c++){
+				int tempR;
+				cin >> tempR;
+				p.required[c] = tempR;
+			}
+		}
+
+		for(int i=0; i<numProc; i++){
+			cout << "Program " << i+1 << ": " << endl;
+			for(int j=0; j<numType; j++){
+				cout << "Current: " << list[i].current.[c] << " ";
+			}
+			cout << endl;
+			for(int j=0; j<numType; j++){
+				cout << "Required: " << list[i].required.[c] << " ";
+			}
+			cout << endl;
 		}
 	}
 
